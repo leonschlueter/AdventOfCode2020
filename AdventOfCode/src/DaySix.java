4fe0e5cb08689be4c18c2e24c2995e4dd231cffa
Leon <Leon@DESKTOP-9PPMKUP>
2020-12-18 15:15:22 +0100

    DaySix added.

---
+++ AdventOfCode/src/DaySix.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DaySix {
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(new File("05DayInput"));
+		ArrayList<String> array = readInput(scanner);
+		scanner.close();
+	}
+
+	private static ArrayList<String> readInput(Scanner scanner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
